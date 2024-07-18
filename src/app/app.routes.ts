1203b4c29a919c1e54e5b6f8bc76319babd0b340
keizeeee <99263770+keizeeee@users.noreply.github.com>
2024-07-18 18:24:45 +0800

    add components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home/home-page/home-page.component';
+import { AboutPageComponent } from './about/about-page/about-page.component';
+import { ExperiencePageComponent } from './experience/experience-page/experience-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomePageComponent,
+        title: 'Home'
+    },
+    {
+        path:'about',
+        component: AboutPageComponent,
+        title: 'About'
+    },
+    {
+        path:'experience',
+        component: ExperiencePageComponent,
+        title: 'Experience'
+    }
+];
