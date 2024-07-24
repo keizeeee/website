af449a58858c6a9690aafc9db05ea3a21a82e3b2
keizeeee <99263770+keizeeee@users.noreply.github.com>
2024-07-24 16:32:34 +0800

    feat: new bg and color change navbar

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../footer/footer.component';
+import { AboutPageComponent } from '../../about/about-page/about-page.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, AboutPageComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
