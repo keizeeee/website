6036d1913a557f40df50d76ad3b00032e0cf3687
keizeeee <99263770+keizeeee@users.noreply.github.com>
2024-07-24 21:47:16 +0800

    feat: add container and navs for experience and projexts page

--- src/app/experience/experience-page/experience-page.component.ts
+++ src/app/experience/experience-page/experience-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HomePageComponent } from '../../home/home-page/home-page.component';
 
 @Component({
   selector: 'app-experience-page',
   standalone: true,
-  imports: [],
+  imports: [HomePageComponent],
   templateUrl: './experience-page.component.html',
   styleUrl: './experience-page.component.css'
 })
