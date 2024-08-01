4c8034b4158c0b9686668358b2671f4e62650c6c
keizeeee <99263770+keizeeee@users.noreply.github.com>
2024-08-01 18:11:18 +0800

    feat: change into scrollable view

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../footer/footer.component';
 import { AboutPageComponent } from '../../about/about-page/about-page.component';
+import { ExperiencePageComponent } from '../../experience/experience-page/experience-page.component';
+import { NavbarComponent } from '../../navbar/navbar.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [FooterComponent, AboutPageComponent],
+  imports: [FooterComponent, AboutPageComponent, ExperiencePageComponent, NavbarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  
 
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HomePageComponent } from '../home/home-page/home-page.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HomePageComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  toHome() {
+    const homeElement = document.getElementById("home");
+    if (homeElement) {
+      homeElement.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      console.error('Element with ID "home" not found.');
+    }
+  }
+
+  toAbout() {
+    const aboutElement = document.getElementById("about");
+    if (aboutElement) {
+      aboutElement.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      console.error('Element with ID "about" not found.');
+    }
+  }
+
+  toExperience() {
+    const experienceElement = document.getElementById("experience");
+    if (experienceElement) {
+      experienceElement.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      console.error('Element with ID "experience" not found.');
+    }
+  }
 
 }
